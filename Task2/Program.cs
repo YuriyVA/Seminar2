81559b70497408af7e65b6de95ee211ce5157bca
YuriyVA <yuriy.ramazanov@gmail.com>
2023-03-11 18:33:25 +0300

    2nd_task

---
+++ Task2/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Введите трехзначное число ");
+int num = int.Parse(Console.ReadLine()!);
+int a = 0;
+if (100 < num && num<999){
+    a = (num%100)%10;
+Console.Write($"{a}");}
+else {Console.Write("третьей цифры нет");}
