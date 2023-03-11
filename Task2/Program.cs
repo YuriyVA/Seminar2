6ecbe5bff3185ade8bae40e89d93d4d2ba1579df
YuriyVA <yuriy.ramazanov@gmail.com>
2023-03-11 18:40:06 +0300

    2nd_task_num_adedd

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -3,5 +3,5 @@ int num = int.Parse(Console.ReadLine()!);
 int a = 0;
 if (100 < num && num<999){
     a = (num%100)%10;
-Console.Write($"{a}");}
+Console.Write($"{num} -> {a}");}
 else {Console.Write("третьей цифры нет");}
