31a325af71594e72d322b2f8829cdcd44419cc89
YuriyVA <yuriy.ramazanov@gmail.com>
2023-03-14 22:00:39 +0300

    all

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,9 +1,12 @@
 ﻿Console.WriteLine("Введите число ");
 int num = int.Parse(Console.ReadLine()!);
 
-if (0<num && num<6){
-    Console.WriteLine("Нет");}
+if (0 < num && num < 6)
+{
+    Console.WriteLine("Нет");
+}
 
-if (5<num && num<8){
-    Console.WriteLine("Да");}
-    
+else if (5 < num && num < 8)
+{
+    Console.WriteLine("Да");
+}
