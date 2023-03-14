48a3fa8984a0ca79137bcdcb0ae0383009edf34c
YuriyVA <yuriy.ramazanov@gmail.com>
2023-03-14 22:09:35 +0300

    all

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,6 +1,6 @@
 ﻿Console.Write("Введите трехзначное число ");
 int num = int.Parse(Console.ReadLine()!);
-if (100 < num && num < 1000)
+if (99 < num && num < 1000)
 {
     int result = (num % 100) / 10;
     Console.Write($"{num} -> {result}");
--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,7 +1,18 @@
-﻿Console.Write("Введите трехзначное число ");
+﻿Console.Write("Введите число ");
 int num = int.Parse(Console.ReadLine()!);
 int a = 0;
-if (100 < num && num<999){
-    a = (num%100)%10;
-Console.Write($"{num} -> {a}");}
-else {Console.Write("третьей цифры нет");}
+if (99 < num)
+{
+    while (num > 999)
+    {
+        num /= 10;
+    }
+
+    a = num % 10;
+    
+    Console.Write($"{num} -> {a}");
+}
+else
+{
+    Console.Write("третьей цифры нет");
+}
